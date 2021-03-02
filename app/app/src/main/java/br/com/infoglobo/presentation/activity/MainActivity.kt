7f8e00545ac5498208140d5c6498c018ed0829c4
Thiago Nicácio <tgomesnicacio@gmail.com>
2021-03-02 20:04:39 -0300

    criando o navigation graph e o fragment home

---
+++ app/app/src/main/java/br/com/infoglobo/presentation/activity/MainActivity.kt
@@ -0,0 +1,23 @@
+package br.com.infoglobo.presentation.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import br.com.infoglobo.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController : NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+}
---
+++ app/app/src/main/java/br/com/infoglobo/presentation/fragment/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package br.com.infoglobo.presentation.fragment
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
