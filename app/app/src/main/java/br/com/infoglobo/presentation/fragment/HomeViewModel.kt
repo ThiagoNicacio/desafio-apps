f9370cb4879539a096b84b8b1ab324e9a0c30e39
Thiago Nicácio <tgomesnicacio@gmail.com>
2021-03-02 22:32:22 -0300

    configurando o viewmodel e o fragment da home

--- app/app/src/main/java/br/com/infoglobo/presentation/fragment/HomeViewModel.kt
+++ app/app/src/main/java/br/com/infoglobo/presentation/fragment/HomeViewModel.kt
@@ -1,7 +1,8 @@
 package br.com.infoglobo.presentation.fragment
 
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel @ViewModelInject internal constructor() : ViewModel() {
     // TODO: Implement the ViewModel
 }
