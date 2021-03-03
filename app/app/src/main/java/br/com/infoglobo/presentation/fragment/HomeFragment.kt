00d986d34986a724c4d8570fe1d3776a8b9fee4e
Thiago Nicácio <tgomesnicacio@gmail.com>
2021-03-02 22:34:49 -0300

    adicionando o click no item de menu

--- app/app/src/main/java/br/com/infoglobo/presentation/fragment/HomeFragment.kt
+++ app/app/src/main/java/br/com/infoglobo/presentation/fragment/HomeFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import br.com.infoglobo.R
 import br.com.infoglobo.databinding.HomeFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -23,6 +25,8 @@ class HomeFragment : Fragment() {
         binding.let {
             it.lifecycleOwner = this
         }
+
+        binding.toolbarMenu.setOnClickListener { Toast.makeText(requireContext(), getString(R.string.in_progress), Toast.LENGTH_SHORT).show() }
 
         return binding.root
     }
